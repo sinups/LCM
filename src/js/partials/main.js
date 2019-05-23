108af91223d470fe61e28c436bdd3ef9c83761cc
Andrei Cojocari <webtheory@mail.ru>
2019-05-23 12:37:39 +0300

    update

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -132,7 +132,7 @@ $(function() {
     autoplay: true,
     autoplaySpeed: 4000,
     lazyLoad:"progressive",
-    speed:600,
+    speed:800,
     arrows:false,
     dots:false,
     cssEase:"cubic-bezier(0.87, 0.03, 0.41, 0.9)",
